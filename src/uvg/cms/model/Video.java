c57e5fd94bc23353816967c2120da4f4edc2d678
Camila <cdgolondrina@gmail.com>
2025-11-12 17:11:12 -0600

    clase vídeo

--- src/uvg/cms/model/Video.java
+++ src/uvg/cms/model/Video.java
@@ -1,5 +1,24 @@
 package model;
 
-public class Video {
+public class Video extends Contenido {
+    private String url;
+    private int duracion;
+
+    //constructor
+    public Video(int id, String titulo, String categoria, String autor, String url, int duracion) {
+        super(id, titulo, categoria, autor);
+        this.url = url;
+        this.duracion = duracion;
+    }
+
+    @Override
+    public void publicar(){
+        System.out.println("Video publicado: " + titulo + " por " + autor);
+    }
+
+    @Override
+    public void visualizar(){
+        System.out.println("Reproduciendo video desde: " + url + " (Duración: " + duracion + " segundos)");
+    }
     
 }
